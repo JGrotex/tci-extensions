2d0d73983d5a54f911c8b9553d7287c0deb5b294
Jörg Grote <joerg@grote-online.de>
2018-01-29 12:11:22 +0100

    first version
    tested and working

---
+++ Nexmo/activity/sms/activity_test.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2018. TIBCO Software Inc. [JGR]
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+package sms
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/stretchr/testify/assert"
+)
+
+var activityMetadata *activity.Metadata
+var connectionData = ``
+
+func getActivityMetadata() *activity.Metadata {
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+	return activityMetadata
+}
+
+func TestActivityRegistration(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	if act == nil {
+		t.Error("Activity Not Registered")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(act.Metadata())
+
+	// *** for testing, replace all in <> with your Account Details!
+
+	//setup attrs
+	tc.SetInput("apiKey", "***REMOVED***")
+	tc.SetInput("apiSecret", "***REMOVED***")
+	tc.SetInput("FromNumber", "NEXMO")
+	tc.SetInput("ToNumber", "***REMOVED***")
+	tc.SetInput("SMStext", "hi from GODev")
+
+	_, err := act.Eval(tc)
+	assert.Nil(t, err)
+
+	result := tc.GetOutput("send")
+	assert.Equal(t, true, result)
+
+	t.Log(result)
+}
